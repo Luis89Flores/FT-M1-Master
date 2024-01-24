9c13f4e12d8ec5785bb83e387b445dab8af25bf1
Luis89Flores <luis.flores041989@gmail.com>
2024-01-23 21:27:00 -0600

    Se suben cambios de la Homework Algoritmos II, Archivo homework.js

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,21 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length <= 1) return array;
 
+  let pivot = array.shift();
+  let izq = [];
+  let der = [];
+
+  for (let i = 0; i < array.length; i++) {
+      if(array[i] < pivot){
+        izq.push(array[i])
+      } else {
+        der.push(array[i])
+      }
+  }
+
+  return quickSort(izq).concat(pivot).concat(quickSort(der));
 }
 
 function mergeSort(array) {
@@ -16,6 +30,37 @@ function mergeSort(array) {
   // Tu código:
 
   //okey probemos ahora
+  if(array.length <= 1) return array;
+  let arrayDividido = dividir(array);
+  let mitad1 = arrayDividido[0];
+  let mitad2 = arrayDividido[1];
+
+  return merge(mergeSort(mitad1),mergeSort(mitad2));
+}
+
+function dividir(array){
+  let mitad = Math.floor(array.length / 2);
+  let arr1 =  array.slice(0, mitad);
+  let arr2 = array.slice(mitad);
+  
+  return [arr1, arr2];
+}
+
+function merge(arr1,arr2){
+  let i = 0;
+  let j = 0;
+  let aux = [];
+  while (i < arr1.length && j < arr2.length) {
+    if(arr1[i]<arr2[j]){
+      aux.push(arr1[i]);
+      i++;
+    } else {
+      aux.push(arr2[j]);
+      j++;
+    }
+  }
+
+  return aux.concat(arr1.slice(i)).concat(arr2.slice(j));
 }
 
 // No modificar nada debajo de esta línea
